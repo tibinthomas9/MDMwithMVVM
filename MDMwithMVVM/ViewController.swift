06928c552444a75f9d9b4ca0caf2c6dc75f6bd1e
Tibin Thomas <tibinmutholy@gmail.com>
2022-04-24 18:12:25 -0400

    Initial Commit

---
+++ MDMwithMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MDMwithMVVM
+//
+//  Created by Tibin Thomas on 24/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MDMwithMVVMUITests/MDMwithMVVMUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MDMwithMVVMUITestsLaunchTests.swift
+//  MDMwithMVVMUITests
+//
+//  Created by Tibin Thomas on 24/04/22.
+//
+
+import XCTest
+
+class MDMwithMVVMUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
